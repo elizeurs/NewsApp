d4e90875b1443b74f87411ccf3a62bd0b9d3b531
elizeurs <13ders@gmail.com>
2021-04-14 14:35:51 -0300

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Elizeu RS on 14/04/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
